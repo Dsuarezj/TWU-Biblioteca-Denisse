ba14a85797f81d6a8d1ee67ce5723822127baca6
Dsuarezj <dsuarez@thoughtworks.com>
2016-07-21 19:21:39 -0500

    test if a user is register

---
+++ src/com/twu/biblioteca/Login.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+public class Login {
+    public boolean isUserRegister(String testUser) {
+    return true;
+    }
+}
--- test/com/twu/biblioteca/LoginTest.java
+++ test/com/twu/biblioteca/LoginTest.java
@@ -1,4 +1,27 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LoginTest {
+
+    @Test
+    public void testIfTestUserIsRegister() {
+        String testUser = "test-user";
+        Login login = new Login ();
+
+        boolean isTestUserRegister = login.isUserRegister(testUser);
+
+        assertEquals(true, isTestUserRegister);
+
+    }
+
+
+
+
+    @Test
+    public void testUserPassIsCorrect() {
+    }
+
 }
