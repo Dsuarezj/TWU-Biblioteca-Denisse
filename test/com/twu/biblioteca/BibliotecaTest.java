add8a9ae57fa6a73be037b1ed113fd2f30903bf1
Dsuarezj <dsuarez@thoughtworks.com>
2016-07-25 09:24:14 -0500

    cleaning test

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -16,24 +17,24 @@ public class BibliotecaTest {
 
     private Biblioteca biblioteca = new Biblioteca();
 
-    private ByteArrayOutputStream byteArrayOutputStream;
     private String bookName = "Book name test";
     private String bookAuthor = "Book author Test";
     private int bookYear = 1990;
 
     private boolean stateAvailable = true;
 
-    Item book = new Item(bookName, bookAuthor, bookYear, stateAvailable);
-    Item movie = new Item("Adventure time season 1", "Pendleton Ward", 2010, true);
+    private Item book = new Item(bookName, bookAuthor, bookYear, stateAvailable);
+    private Item movie = new Item("Adventure time season 1", "Pendleton Ward", 2010, true);
 
-    Item availableItem = new Item("Available Item", bookAuthor, bookYear, true);
-    Item unavailableItem = new Item("Un Available Item", bookAuthor, bookYear, false);
+    private Item availableItem = new Item("Available Item", bookAuthor, bookYear, true);
+    private Item unavailableItem = new Item("Un Available Item", bookAuthor, bookYear, false);
 
-    List<Item> allBooks = Arrays.asList(book, availableItem, unavailableItem);
+    private List<Item> allBooks = Arrays.asList(book, availableItem, unavailableItem);
 
 
     @Before
     public void setUp() {
+        ByteArrayOutputStream byteArrayOutputStream;
         byteArrayOutputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(byteArrayOutputStream));
     }
@@ -45,8 +46,8 @@ public class BibliotecaTest {
 
     @Test
     public void testAddBookToTheBiblioteca() {
-        List<Item> testItemList = Arrays.asList(book);
-        biblioteca.addItemsToTheASeccion(testItemList , Biblioteca.ItemType.BOOK );
+        List<Item> testItemList = Collections.singletonList(book);
+        biblioteca.addItemsToTheASeccion(testItemList, Biblioteca.ItemType.BOOK);
         assertEquals(testItemList, biblioteca.getTotalItems(Biblioteca.ItemType.BOOK));
     }
 
@@ -68,15 +69,16 @@ public class BibliotecaTest {
     @Test
     public void testAddMovieToTheBiblioteca() {
         List<Item> testItemList = Arrays.asList(movie);
-        biblioteca.addItemsToTheASeccion(testItemList , Biblioteca.ItemType.MOVIE);
+        biblioteca.addItemsToTheASeccion(testItemList, Biblioteca.ItemType.MOVIE);
         assertEquals(testItemList, biblioteca.getTotalItems(Biblioteca.ItemType.MOVIE));
     }
+
     @Test
     public void testAddMovieAndBookToTheBibliotecaAndOnlyGetMovieList() {
         List<Item> testBookList = Arrays.asList(book);
         List<Item> testMovieList = Arrays.asList(movie);
-        biblioteca.addItemsToTheASeccion(testBookList , Biblioteca.ItemType.BOOK);
-        biblioteca.addItemsToTheASeccion(testMovieList , Biblioteca.ItemType.MOVIE);
+        biblioteca.addItemsToTheASeccion(testBookList, Biblioteca.ItemType.BOOK);
+        biblioteca.addItemsToTheASeccion(testMovieList, Biblioteca.ItemType.MOVIE);
         assertEquals(testMovieList, biblioteca.getTotalItems(Biblioteca.ItemType.MOVIE));
     }
 
--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -9,11 +9,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class UserTest {
-    List<String> masterUserInformation = Arrays.asList("000-0000", "master key", "Maestro", "master@biblioteca.com", "Bangalore", "123-123-123");
-    List<String> firstUserInformation = Arrays.asList("000-0001", "first key", "First User", "first@biblioteca.com", "Bangalore", "123-123-123");
+    private List<String> masterUserInformation = Arrays.asList("000-0000", "master key", "Maestro", "master@biblioteca.com", "Bangalore", "123-123-123");
+    private List<String> firstUserInformation = Arrays.asList("000-0001", "first key", "First User", "first@biblioteca.com", "Bangalore", "123-123-123");
 
-    User masterUser = new User(masterUserInformation);
-    User firstUser = new User(firstUserInformation);
+    private User masterUser = new User(masterUserInformation);
+    private User firstUser = new User(firstUserInformation);
 
     @Test
     public void testUserAreEqualWhenTheInformationIsTheSame() {
@@ -24,5 +24,5 @@ public class UserTest {
     public void testUserAreNotEqualWhenTheIDIsDifferent() {
         assertNotEquals(masterUser, firstUser);
     }
-    
+
 }
