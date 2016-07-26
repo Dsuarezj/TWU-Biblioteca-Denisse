84ef9265f0a3c39ae471a397880864062b9b86e4
Dsuarezj <dsuarez@thoughtworks.com>
2016-07-26 16:37:28 -0500

    refactor BibliotecaTest

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -17,17 +17,11 @@ public class BibliotecaTest {
 
     private Biblioteca biblioteca = new Biblioteca();
 
-    private String bookName = "Book name test";
-    private String bookAuthor = "Book author Test";
-    private int bookYear = 1990;
-
-    private boolean stateAvailable = true;
-
-    private Item book = new Item(bookName, bookAuthor, bookYear, stateAvailable);
+    private Item book = new Item("1Q84", "Haruki Murakami", 2009, true);
     private Item movie = new Item("Adventure time season 1", "Pendleton Ward", 2010, true);
 
-    private Item availableItem = new Item("Available Item", bookAuthor, bookYear, true);
-    private Item unavailableItem = new Item("Un Available Item", bookAuthor, bookYear, false);
+    private Item availableItem = new Item("Available Item", "Some author", 1990, true);
+    private Item unavailableItem = new Item("Un Available Item", "Another Author", 2000, false);
 
     private List<Item> allBooks = Arrays.asList(book, availableItem, unavailableItem);
 
@@ -47,7 +41,9 @@ public class BibliotecaTest {
     @Test
     public void testAddBookToTheBiblioteca() {
         List<Item> testItemList = Collections.singletonList(book);
+
         biblioteca.addItemsToASection(testItemList, Biblioteca.section.BOOK);
+
         assertEquals(testItemList, biblioteca.getTotalItems(Biblioteca.section.BOOK));
     }
 
@@ -55,21 +51,27 @@ public class BibliotecaTest {
     @Test
     public void testReturnAvailableBooks() {
         List<Item> expectedOnlyAvailable = Arrays.asList(book, availableItem);
+
         biblioteca.addItemsToASection(allBooks, Biblioteca.section.BOOK);
+
         assertEquals(expectedOnlyAvailable, biblioteca.getItemsThatAreAvailable(Biblioteca.section.BOOK));
     }
 
     @Test
     public void testReturnNotAvailableBooks() {
         List<Item> expectedNotAvailable = Arrays.asList(unavailableItem);
+
         biblioteca.addItemsToASection(allBooks, Biblioteca.section.BOOK);
+
         assertEquals(expectedNotAvailable, biblioteca.getItemsThatAreNotAvailable(Biblioteca.section.BOOK));
     }
 
     @Test
     public void testAddMovieToTheBiblioteca() {
         List<Item> testItemList = Arrays.asList(movie);
+
         biblioteca.addItemsToASection(testItemList, Biblioteca.section.MOVIE);
+
         assertEquals(testItemList, biblioteca.getTotalItems(Biblioteca.section.MOVIE));
     }
 
@@ -77,8 +79,10 @@ public class BibliotecaTest {
     public void testAddMovieAndBookToTheBibliotecaAndOnlyGetMovieList() {
         List<Item> testBookList = Arrays.asList(book);
         List<Item> testMovieList = Arrays.asList(movie);
+
         biblioteca.addItemsToASection(testBookList, Biblioteca.section.BOOK);
         biblioteca.addItemsToASection(testMovieList, Biblioteca.section.MOVIE);
+
         assertEquals(testMovieList, biblioteca.getTotalItems(Biblioteca.section.MOVIE));
     }
 
